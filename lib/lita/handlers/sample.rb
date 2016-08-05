3d71602e758371cc349978fa0e5831ed3a1dc6cc
fukayatsu <fukayatsu@gmail.com>
2016-08-05 17:56:41 +0900

    Add count option to sample

--- lib/lita/handlers/sample.rb
+++ lib/lita/handlers/sample.rb
@@ -2,11 +2,18 @@ module Lita
   module Handlers
     class Sample < Handler
 
-      route /^sample (.+)/, :sample, command: false,
-        help: { "sample [foo] [bar] [baz]" => "[foo] に決めた！" }
+      route /^sample (?:([0-9]) )?(.+)/, :sample, command: false,
+        help: { 'sample [n] [foo] [bar] [baz]...' => '[foo] に決めた！' }
       def sample(response)
-        list = response.matches[0][0].split
-        response.reply "#{list.sample} に決めた！"
+        total, list = response.matches.flatten
+        total ||= 1
+        list = list.split.shuffle
+
+        total.to_i.times do
+          item = list.pop
+          break unless item
+          response.reply "#{item} に決めた！"
+        end
       end
     end
     Lita.register_handler(Sample)
